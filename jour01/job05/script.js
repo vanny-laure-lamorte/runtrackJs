f7ed442b2d3b8f64e9249d8701f59412a567945f
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-15 10:34:44 +0200

    job05

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+function afficherJoursSemaines() {
+    const jourSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]; 
+
+    for( const i of jourSemaines ) {
+        console.log(i);
+    }
+}
+
+afficherJoursSemaines()
+
