b64ab6744c56854628b79bf2e9d42f939cca7fd4
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-17 11:47:53 +0200

    Modifier le Jour, job

---
+++ jour03/job01/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    //Show
+    $('#citation').hide();
+    $('#btn1').click(function() {
+        $('#citation').show();
+    });
+
+    //Hide
+    $('#btn2').click(function() {
+        $('html').hide();
+    });
+});
---
+++ jour03/job02/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', ev => {
+    var img = [
+        "assets/arc1.png",
+        "assets/arc2.png",
+        "assets/arc3.png",
+        "assets/arc4.png",
+        "assets/arc5.png",
+        "assets/arc6.png"
+    ];
+
+    const TrieImage = kBrouilleArray(ImgPath);
+
+    console.log(Trie);
+
+});
